ba56de46bc71d5cae0f6e74862f5cdc5c3b858e5
Iván Arnaldo Salas <66227125+IvanS4L4S@users.noreply.github.com>
2025-01-31 16:25:57 -0300

    Bootstrap
    se implemento para aumentar la rapidez del diseño.

--- ecoplast-proyect-angular/src/app/app.routes.ts
+++ ecoplast-proyect-angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home-page', pathMatch: 'full' },
+  { path: 'home-page', component: HomeComponent },
+  { path: 'contacto', component: ContactComponent }
+];
